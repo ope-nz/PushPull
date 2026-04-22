992e5f6f297f39211317dda764bfd67259451220
ope-nz <github@ope.nz>
2026-04-22 16:02:43 +1200

    PushPull update

--- _compile.bat
+++ _compile.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /d "D:\Dropbox\My Apps\DotNet\GFD"
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe ^
-  /target:winexe /platform:x64 /out:GFD.exe ^
+  /target:winexe /platform:x64 /out:PushPull.exe ^
   /win32icon:icon.ico ^
   /r:System.dll /r:System.Windows.Forms.dll /r:System.Drawing.dll ^
   /r:System.Net.dll /r:System.Web.Extensions.dll /r:System.Security.dll ^
