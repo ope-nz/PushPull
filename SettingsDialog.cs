50d9003fc1bcc49fb7707a59178abbaf4e12c0eb
ope-nz <github@ope.nz>
2026-04-22 18:25:37 +1200

    PushPull update

--- SettingsDialog.cs
+++ SettingsDialog.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
 
 namespace PushPull
@@ -7,6 +8,7 @@ namespace PushPull
     {
         GfdConfig _config;
         TextBox txtToken;
+        TextBox txtDefaultIgnore;
 
         public SettingsDialog(GfdConfig config)
         {
@@ -20,7 +22,7 @@ namespace PushPull
             this.Text = "Settings";
             this.FormBorderStyle = FormBorderStyle.FixedDialog;
             this.StartPosition = FormStartPosition.CenterParent;
-            this.ClientSize = new System.Drawing.Size(480, 120);
+            this.ClientSize = new System.Drawing.Size(480, 260);
             this.MinimizeBox = false;
             this.MaximizeBox = false;
 
@@ -35,17 +37,40 @@ namespace PushPull
             var btnShow = new Button { Text = "Show", Location = new System.Drawing.Point(398, 33), Width = 60 };
             btnShow.Click += (s, e) => { txtToken.UseSystemPasswordChar = !txtToken.UseSystemPasswordChar; btnShow.Text = txtToken.UseSystemPasswordChar ? "Show" : "Hide"; };
 
-            var btnTest = new Button { Text = "Test Connection", Location = new System.Drawing.Point(12, 75), Width = 120 };
+            var lblIgnore = new Label { Text = "Default Ignore Patterns:", Location = new System.Drawing.Point(12, 75), AutoSize = true };
+            var lblIgnoreHint = new Label { Text = "(applied to new projects, one per line)", Location = new System.Drawing.Point(175, 75), AutoSize = true, ForeColor = System.Drawing.Color.Gray };
+            txtDefaultIgnore = new TextBox
+            {
+                Location = new System.Drawing.Point(12, 95),
+                Width = 446,
+                Height = 100,
+                Multiline = true,
+                AcceptsReturn = true,
+                ScrollBars = ScrollBars.Vertical,
+                Text = _config.DefaultIgnorePatterns != null ? string.Join("\r\n", _config.DefaultIgnorePatterns) : ""
+            };
+
+            var btnTest = new Button { Text = "Test Connection", Location = new System.Drawing.Point(12, 215), Width = 120 };
             btnTest.Click += (s, e) => TestConnection();
 
-            var btnOk = new Button { Text = "OK", DialogResult = DialogResult.OK, Location = new System.Drawing.Point(300, 75), Width = 75 };
-            var btnCancel = new Button { Text = "Cancel", DialogResult = DialogResult.Cancel, Location = new System.Drawing.Point(382, 75), Width = 75 };
+            var btnOk = new Button { Text = "OK", DialogResult = DialogResult.OK, Location = new System.Drawing.Point(300, 215), Width = 75 };
+            var btnCancel = new Button { Text = "Cancel", DialogResult = DialogResult.Cancel, Location = new System.Drawing.Point(382, 215), Width = 75 };
 
-            btnOk.Click += (s, e) => { _config.Token = txtToken.Text.Trim(); };
+            btnOk.Click += (s, e) =>
+            {
+                _config.Token = txtToken.Text.Trim();
+                var patterns = new List<string>();
+                foreach (string line in txtDefaultIgnore.Text.Split('\n'))
+                {
+                    string t = line.Trim();
+                    if (t.Length > 0) patterns.Add(t);
+                }
+                _config.DefaultIgnorePatterns = patterns.Count > 0 ? patterns : null;
+            };
 
             this.AcceptButton = btnOk;
             this.CancelButton = btnCancel;
-            this.Controls.AddRange(new Control[] { lblToken, txtToken, btnShow, btnTest, btnOk, btnCancel });
+            this.Controls.AddRange(new Control[] { lblToken, txtToken, btnShow, lblIgnore, lblIgnoreHint, txtDefaultIgnore, btnTest, btnOk, btnCancel });
         }
 
         void TestConnection()
