dce1aa943a9d9fd2ede81fa3f0ca0d0d9e6edd3d
ope-nz <github@ope.nz>
2026-04-25 11:53:01 +1200

    Added default repo owner in config and push all with comment.

--- _compile.bat
+++ _compile.bat
@@ -7,7 +7,7 @@ C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe ^
   /r:System.Net.dll /r:System.Web.Extensions.dll /r:System.Security.dll ^
   /r:Microsoft.VisualBasic.dll ^
   Program.cs GitHub.cs ConfigManager.cs SyncEngine.cs AppIcon.cs ^
-  SettingsDialog.cs ProjectDialog.cs ^
+  SettingsDialog.cs ProjectDialog.cs CommentDialog.cs ^
   MainForm.Designer.cs MainForm.cs
 echo EXIT CODE: %ERRORLEVEL%
 
